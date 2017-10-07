a494e13b4db8938a56afcdcc5412cc7bb5ecadab
Huang731 <32349507+Huang731@users.noreply.github.com>
2017-10-07 21:58:37 +0800

    haha
    fjdlsflkjsa

---
+++ bos_management/src/main/java/cn/itcast/bos/dao/base/StandardRepository.java
@@ -0,0 +1,10 @@
+package cn.itcast.bos.dao.base;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cn.itcast.bos.domain.base.Standard;
+
+public interface StandardRepository extends JpaRepository<Standard, Integer>{
+
+
+}
---
+++ bos_management/src/main/java/cn/itcast/bos/service/StandardService.java
@@ -0,0 +1,12 @@
+package cn.itcast.bos.service;
+
+import cn.itcast.bos.domain.base.Standard;
+
+/**
+ * 收派标准管理
+ * @author eric
+ *
+ */
+public interface StandardService {
+	public void save(Standard standard);
+}
